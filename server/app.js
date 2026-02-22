859c664a8fc5323c2610b95eb55bfee137e8801a
nmatseke <nmatseke@finchoice.co.za>
2026-02-23 00:10:49 +0200

    fix app for backend serving react

--- server/app.js
+++ server/app.js
@@ -17,18 +17,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
-// Serve React static files
-app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
-// Catch-all handler
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+}
 
 module.exports = app;
