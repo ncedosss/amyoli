9786e6339d288c886771d1e8d670d39a33033880
nmatseke <nmatseke@finchoice.co.za>
2026-03-19 06:51:26 +0200

    Fix invoice for painters

--- server/utils/invoice.js
+++ server/utils/invoice.js
@@ -95,19 +95,20 @@ function generateInvoice(invoiceData) {
       doc.text("Atlantis Industria, Atlantis", 14, 85);
       doc.text("7349", 14, 90);
       doc.text("Vat No. 4950102162", 14, 95);
-    } else if (invoiceData.client === "CSV" || invoiceData.client === "Lesedi CSV" || invoiceData.client === 'Lesedi Ellegent 850') {
+    } else if (invoiceData.client === "CSV" || invoiceData.client === "Lesedi CSV" || invoiceData.client === 'Lesedi Ellegent 850' || 'Lesedi Painters R400') {
       doc.text("ATT: Errol Sedras", 14, 70);
       doc.text("CSV Construction (PTY) LTD", 14, 75);
       doc.text("487 Old Main Road", 14, 80);
       doc.text("Firgrove, Somerset West", 14, 85);
       doc.text("7130", 14, 90);
-    } else if (invoiceData.client === "Lesedi Painters R400") {
-      doc.text("ATT: Wendy Morgan", 14, 70);
-      doc.text("LESEDI NUCLEAR SERVICES PTY LTD", 14, 75);
-      doc.text("Cape Town", 14, 80);
-      doc.text("South Africa", 14, 85);
-      doc.text("VAT No. 4460195755", 14, 90);
-    }
+    } 
+    // else if (invoiceData.client === "Lesedi Painters R400") {
+    //   doc.text("ATT: Wendy Morgan", 14, 70);
+    //   doc.text("LESEDI NUCLEAR SERVICES PTY LTD", 14, 75);
+    //   doc.text("Cape Town", 14, 80);
+    //   doc.text("South Africa", 14, 85);
+    //   doc.text("VAT No. 4460195755", 14, 90);
+    // }
 
     // Right side INVOICE details
     doc.text(`${invoiceData.invoiceNo || ""}`, 112, 66);
@@ -143,6 +144,13 @@ function generateInvoice(invoiceData) {
     }else if (invoiceData.client === "Lesedi CSV") {
       doc.text(
         "EMPLOYEE TRANSPORT - SAMORA ROUTE",
+        195,
+        95,
+        { align: "right" }
+      );
+    }else if (invoiceData.client === "Lesedi Painters R400") {
+      doc.text(
+        "NB: WVC (Painters) POL059871",
         195,
         95,
         { align: "right" }
