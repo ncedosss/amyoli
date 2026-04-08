31fb50571e08b31e26048b609c80f82e433bd8a2
nmatseke <nmatseke@finchoice.co.za>
2026-04-08 16:47:53 +0200

    Use exceljs instead

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,16 +1,16 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } 
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: { rejectUnauthorized: false } 
+// });
 
 module.exports = pool;
