ed4c66b0cb728b6e1b735e285d27a6295c51d40f
nmatseke <nmatseke@finchoice.co.za>
2026-04-30 12:28:25 +0200

    Fix adhoc invoice no

--- server/utils/invoice.js
+++ server/utils/invoice.js
@@ -112,7 +112,7 @@ function generateInvoice(invoiceData) {
     }
 
     // Right side INVOICE details
-    doc.text(`${invoiceData.invoiceNo || ""}.1`, 112, 66);
+    doc.text(`${invoiceData.invoiceNo || ""}`, 112, 66);
     doc.text(new Date().toLocaleDateString(), 157, 66);
 
     if (invoiceData.client === "Atlantis Foundaries") {
